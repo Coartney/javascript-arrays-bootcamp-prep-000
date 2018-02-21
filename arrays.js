57446130c5556c027389eec2933b1d6d49c938d7
coartney22@gmail.com <coartney22@gmail.com>
2018-02-21 03:24:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,7 +43,10 @@ function destructivelyRemoveElementFromEndOfArray(array){
 }
 
 
-
+function removeElementFromEndOfArray(array){
+  onlyTemporary = array;
+  return onlyTemporary.slice(0, onlyTemporary.length - 1)
+}
 
 
 
