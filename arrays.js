48290e0080208da106df52f118673cf34e1963a9
coartney22@gmail.com <coartney22@gmail.com>
2018-02-21 02:19:23 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,11 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element){
+  var addElementToBeginningOfArray = [element, ...array];
+  return addElementToBeginningOfArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  const destructivelyAddElementToBeginningOfArray = [element, ...array];
+  return destructivelyAddElementToBeginningOfArray
+}
