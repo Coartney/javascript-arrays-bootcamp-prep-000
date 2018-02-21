5d7494447bd169a5b832fd88f11fa4d90180e81b
coartney22@gmail.com <coartney22@gmail.com>
2018-02-21 02:57:14 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,6 @@ function addElementToBeginningOfArray(array, element){
 function destructivelyAddElementToBeginningOfArray(array, element){
 var newArray = array;
 newArray = [element, ...newArray]
-newArray;
+newArray = newArray
 return newArray
 }
